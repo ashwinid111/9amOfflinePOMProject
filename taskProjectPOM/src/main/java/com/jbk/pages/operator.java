cd4ead5477ba94885d6cb7a9e2e87b94b0855164
Arya <Arya@Arya-PC.mshome.net>
2020-09-03 10:34:17 +0530

    added operator file by shailesh

---
+++ taskProjectPOM/src/main/java/com/jbk/pages/operator.java
@@ -0,0 +1,67 @@
+package com.jbk.pages;
+	import java.util.ArrayList;
+	import java.util.Collections;
+	import java.util.List;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.WebElement;
+	import org.openqa.selenium.support.PageFactory;
+	import com.aventstack.extentreports.ExtentTest;
+	import com.jbk.repository.OperatorPageRepository;
+	public class operator extends OperatorPageRepository {
+
+		public operator(WebDriver driver) {
+				PageFactory.initElements(driver, this);
+			}
+		public void NavigateToAddUserPage() {
+				email.clear();
+				email.sendKeys("kiran@gmail.com");
+				password.clear();
+				password.sendKeys("123456");
+				loginButton.click();
+				operators.click();
+			}
+		public boolean onecell(ExtentTest test1) {
+			test1.info("My First Test Case");
+			String actPerson = tableCell.getText();
+			String expPerson = "Enquiry(Course Details, Fees, Enquiry)";
+			if (actPerson.equals(expPerson)) {
+				return true;
+			} else {
+				return false;
+			}}
+		public boolean validateHeader(ExtentTest test2) {
+			test2.info("My Second Test Case");
+			List<WebElement> listOfHeader = headers;
+			if (listOfHeader.size() == 6) {
+				return true;
+			} else {
+				return false;
+			}}
+		
+		public boolean validateColumn(ExtentTest test3) {
+			test3.info("My Third Test Case");
+			List<WebElement> listOfColumn = tableRow;
+			if (listOfColumn.size() == 5) {
+				return true;
+			} else {
+				return false;
+			}}
+		public boolean checkPersonListIsSort(ExtentTest test4) {
+			test4.info("My Fourth Test Case");
+			ArrayList<String> actualWebList = new ArrayList<>();
+			List<WebElement> elementList = tablePersonList;
+			for (WebElement we : elementList) {
+				actualWebList.add(we.getText());
+			}
+			ArrayList<String> sortedList = new ArrayList<>();
+			for (String s : actualWebList) {
+				sortedList.add(s);
+			}
+			Collections.sort(sortedList);
+			if (sortedList.equals(actualWebList)) {
+				return true;
+			} else {
+				return false;
+			}}
+		}
+	
