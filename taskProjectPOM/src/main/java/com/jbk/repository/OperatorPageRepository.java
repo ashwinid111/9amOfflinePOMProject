55a3d957c3aed7dcea3cbc0c002ba3c687e0e2f9
Arya <Arya@Arya-PC.mshome.net>
2020-09-04 09:59:41 +0530

    added by shailesh

--- taskProjectPOM/src/main/java/com/jbk/repository/OperatorPageRepository.java
+++ taskProjectPOM/src/main/java/com/jbk/repository/OperatorPageRepository.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 public class OperatorPageRepository {
-
+//xpath
 	WebDriver driver;
 	@FindBy(id = "email")
 	public WebElement email;
