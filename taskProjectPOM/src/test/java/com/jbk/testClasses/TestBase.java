24b4c30a1298a47cad0ab4ec7159d7289ab8601b
Arya <Arya@Arya-PC.mshome.net>
2020-09-04 10:23:46 +0530

    Config Properties & Test Base (Extent Report) added by Shailesh

--- taskProjectPOM/src/test/java/com/jbk/testClasses/TestBase.java
+++ taskProjectPOM/src/test/java/com/jbk/testClasses/TestBase.java
@@ -1,20 +1,25 @@
 package com.jbk.testClasses;
-
 import java.io.FileInputStream;
 import java.util.Properties;
-
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.FindBy;
-
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 import com.jbk.pages.LoginPage;
-
 public class TestBase {
 	public static WebDriver driver;
 	Properties prop = null;
 	FileInputStream fis = null;
 	LoginPage lp=null;
+	
+//extent report variables
+	
+	public ExtentHtmlReporter htmlReporter;
+	public ExtentReports extent;
+	public ExtentTest test;
+	public ExtentTest testPrePage;
 	
 	String readAnyProperty( String propName) {
 		String val = null;
@@ -36,10 +41,8 @@ public class TestBase {
 		driver.get(URL);
 		driver.manage().window().maximize();
 		lp=new LoginPage(driver);
-		
 		return driver;
 	}
-	
 	public LoginPage loadLoginpage(){
 		
 		LoginPage lp=new LoginPage(driver);
@@ -49,5 +52,28 @@ public class TestBase {
 		driver.close();
 	}
 
+	//Extent Report Code
+	
+	public ExtentReports setReport(String pageName, String extentReprtName, String hostName, String environment, String uName,
+			String docTitle, String reportName) {
 
+		htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "/test-output/" + extentReprtName + ".html");
+		htmlReporter.config().setDocumentTitle(docTitle);
+		htmlReporter.config().setReportName(reportName);
+		htmlReporter.config().setTheme(Theme.STANDARD);
+		extent = new ExtentReports();
+		extent.attachReporter(htmlReporter);
+		extent.setSystemInfo("Host Name", hostName);
+		extent.setSystemInfo("Environment", environment);
+		extent.setSystemInfo("User Name", uName);
+		test=extent.createTest("All TestCases pages", "TestCases");
+		return extent;
+	}
+	public void endReport() {
+		extent.flush();
+	}
+	public void extenttestNode() {
+		testPrePage=test.createNode("OperatorPage");
+	}
+	
 }
